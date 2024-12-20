e44ee70bf4517e34798bc68eac297675ea662fbc
Kelniit <kelpmul01@gmail.com>
2024-12-20 19:05:06 +0700

    Hi !

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 function GenerateStack() {
-  let techno = ["python", "tensorflow", "docker", "mysql", "flask"]
+  let techno = ["python", "tensorflow", "mysql", "flask"]
   let othertech = ["googlecloud", "googlebigquery", "googledataflow"]
-  let techstack = [...techno, ...othertech]
+  let techtech = ["terraform", "kubernetes", "docker"]
+  let techstack = [...techno, ...othertech, ...techtech]
   const mainrow = document.querySelector("#techside")
   techstack.forEach(tech => {
     const colDiv = document.createElement("div");
     colDiv.className = "col-4 col-md-1";
 
     const img = document.createElement("img");
-    img.src = `https://cdn.simpleicons.org/${tech}/black`;
+    img.src = `https://cdn.simpleicons.org/${tech}/white`;
 
     colDiv.appendChild(img);
     mainrow.appendChild(colDiv);
